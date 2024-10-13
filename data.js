bfa8c1c971ec0665bad9939f5f0c50886a8616f0
unknown <goh_jia_wei@hotmail.com>
2024-10-13 14:55:22 +0800

    Edit data, add text styling

--- data.js
+++ data.js
@@ -2,15 +2,15 @@ var data = [
     {
         header: "WORKFLOW.",
         records: [
-            ["git checkout master", "Always start from master branch."],
+            ["git checkout <i>master</i>", "Always start from master branch."],
             ["git pull", "Update your local Git repository with any changes from remote."],
-            ["git checkout -b branch_name", "Create local feature branch."],
-            ["git add file_name", "Stage a file."],
-            ["git commit -m commit_name", "Make a new commit"],
-            ["git push origin branch_name", "Push branch to remote (for code review and merge)."],
-            ["git checkout master", "Alternatively, merge to local master branch and push to remote."],
-            ["git merge branch_name", "Merge local feature branch to local master branch"],
-            ["git push", "Push master branch to remote"]
+            ["git checkout -b <i>branch_name</i>", "Create local feature branch."],
+            ["git add <i>file_name</i>", "Stage a file."],
+            ["git commit -m <i>commit_name</i>", "Make a new commit."],
+            ["git push origin <i>branch_name</i>", "Push branch to remote (for code review and merge)."],
+            ["git checkout <i>master</i>", "Alternatively, merge to local master branch and push to remote."],
+            ["git merge <i>branch_name</i>", "Merge local feature branch to local master branch."],
+            ["git push", "Push master branch to remote."]
         ]
     },
     {
@@ -18,12 +18,14 @@ var data = [
         records: [
             ["git status", "View currently staged, modified and untracked files."],
             ["git commit --amend --no-edit", "Modify current commit."],
-            ["git rebase master", "Rebase your local branch with changes in the local master branch"],
+            ["git rebase <i>master</i>", "Rebase your local branch with changes in the local master branch."],
+            ["git rebase -i HEAD~<i>3</i>", "Interactive rebase for the last 3 commits."],
             ["git reset HEAD~1", "Reset latest commit (file changes become unstaged)."],
-            ["git reset HEAD~1 --hard", "Reset latest commit as well as remove all file changes (CAUTION!)."],
-            ["git restore <filename>", "Reset current changes to a file (CAUTION!)."],
+            ["git reset HEAD~1 --hard", "Reset latest commit as well as remove all file changes <b>CAUTION</b>."],
+            ["git restore <i>filename</i>", "Reset current changes to a file <b>CAUTION!</b>."],
             ["git reset [--hard] <commit-before-merge>", "Reset to a commit before a merge."],
-            ["git revert -m 1 <merge-commit-hash>", "Revert a merge commit."]
+            ["git revert -m 1 <merge-commit-hash>", "Revert a merge commit."],
+            ["git cherry-pick <i>commit_hash<i>", "Apply the changes introduced by an existing commit."],
         ]
     },
     {
@@ -31,12 +33,12 @@ var data = [
         records: [
             ["git branch", "List all local branches."],
             ["git branch -a", "List remote and local branches."],
-            ["git checkout -b branch_name", "Create a local branch and switch to it."],
-            ["git checkout branch_name", "Switch to an existing branch."],
-            ["git push origin branch_name", "Push branch to remote."],
-            ["git branch -m new_name", "Rename current branch."],
-            ["git branch -d branch_name", "Delete a local branch."],
-            ["git push origin :branch_name", "Delete a remote branch."]
+            ["git checkout -b <i>branch_name</i>", "Create a local branch and switch to it."],
+            ["git checkout <i>branch_name</i>", "Switch to an existing branch."],
+            ["git push <i>origin</i> <i>branch_name</i>", "Push branch to remote."],
+            ["git branch -m <i>new_name</i>", "Rename current branch."],
+            ["git branch -d <i>branch_name</i>", "Delete a local branch."],
+            ["git push -d  <i>origin</i> <i>branch_name</i>", "Delete a remote branch."]
         ]
     },
     {
@@ -46,9 +48,9 @@ var data = [
             ["git log -2", "Show commit history for last N commits."],
             ["git log -p -2", "Show commit history for last N commits with diff."],
             ["git diff", "Show all local file changes in the working tree."],
-            ["git diff myfile", "Show changes made to a file."],
-            ["git blame myfile", "Show who changed what & when in a file."],
-            ["git remote show origin", "Show remote branches and their mapping to local."]
+            ["git diff <i>filename</i>", "Show changes made to a file."],
+            ["git blame <i>filename</i>", "Show who changed what & when in a file."],
+            ["git remote show <i>origin</i>", "Show remote branches and their mapping to local."]
         ]
     },
     {
@@ -57,17 +59,7 @@ var data = [
             ["git clean -f", "Delete all untracked files."],
             ["git clean -df", "Delete all untracked files and directories."],
             ["git checkout -- .", "Undo local modifications to all files."],
-            ["git reset HEAD myfile", "Unstage a file."]
-        ]
-    },
-    {
-        header: "TAGS.",
-        records: [
-            ["git pull --tags", "Get remote tags."],
-            ["git checkout tag_name", "Switch to an existing tag."],
-            ["git tag", "List all tags."],
-            ["git tag -a tag_name -m 'tag message'", "Create a new tag."],
-            ["git push --tags", "Push all tags to remote repo."]
+            ["git reset HEAD <i>filename</i>", "Unstage a file."]
         ]
     },
     {
@@ -75,11 +67,21 @@ var data = [
         records: [
             ["git stash", "Save existing changes to a stash."],
             ["git stash pop", "Apply a stash and delete it from stash list."],
-            ["git stash save 'stash name' && git stash", "Save changes to a named stash."],
+            ["git stash save '<i>stash name</i>' && git stash", "Save changes to a named stash."],
             ["git stash list", "List all stashes."],
         
         ]
     },
+    {
+        header: "TAGS.",
+        records: [
+            ["git pull --tags", "Get remote tags."],
+            ["git checkout <i>tag_name</i>", "Switch to an existing tag."],
+            ["git tag", "List all tags."],
+            ["git tag -a <i>tag_name</i> -m '<i>tag message</i>'", "Create a new tag."],
+            ["git push --tags", "Push all tags to remote repo."]
+        ]
+    }
 ];
 
 window.data = data;
